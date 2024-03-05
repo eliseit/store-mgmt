0bddcf3d7bbf493f7ccdda2344a546d899481be8
Elisei TATARU <elisei.tataru@gmail.com>
2024-03-05 09:37:20 +0200

    Initial commit - Maven Spring Boot setup

---
+++ src/main/java/ro/etataru/storemgmt/StoreMgmtApplication.java
@@ -0,0 +1,13 @@
+package ro.etataru.storemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/etataru/storemgmt/StoreMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.etataru.storemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
