e5566abde69c02f5c57df11d98810ad81607d1b5
Elisei TATARU <elisei.tataru@gmail.com>
2024-03-06 16:44:18 +0200

    Add Rest Controller and Integration test

---
+++ src/main/java/ro/etataru/storemgmt/web/controllers/ProductsController.java
@@ -0,0 +1,18 @@
+package ro.etataru.storemgmt.web.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductsController {
+
+    @GetMapping()
+    public List<String> getAllProducts() {
+        return List.of("Product 1", "Product 2");
+    }
+}
--- src/test/java/ro/etataru/storemgmt/StoreMgmtApplicationTests.java
+++ src/test/java/ro/etataru/storemgmt/StoreMgmtApplicationTests.java
@@ -1,13 +1,41 @@
 package ro.etataru.storemgmt;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ro.etataru.storemgmt.web.controllers.ProductsController;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class StoreMgmtApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    ProductsController productsController;
+    private MockMvc mockMvc;
 
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(this.productsController).build();
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+        mockMvc.perform(get("/product"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2)))
+        ;
+    }
 }
